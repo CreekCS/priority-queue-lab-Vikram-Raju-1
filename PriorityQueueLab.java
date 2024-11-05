9c455215efcfcf35d31965bdb8fdaadd48f68baa
Vikram Raju <178485086+Vikram-Raju-1@users.noreply.github.com>
2024-11-05 20:29:46 +0000

    First commit (working solution)

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,20 @@
 // import statements
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.PriorityQueue;
 
 public class PriorityQueueLab {
-  
+    public static void main(String[] args) throws FileNotFoundException {
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+        try {
+            File randIntegers = new File("RandIntegers.txt");   
+            Scanner scan = new Scanner(randIntegers);
+            while(scan.hasNextInt()) queue.add((scan.nextInt()));
+            while (!queue.isEmpty()) System.out.println(queue.poll());    
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } 
+    }
 }
 
